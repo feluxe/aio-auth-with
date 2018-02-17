7c5a0f43a46e04ffe5df121bb4326ee525f3a79b
feluxe <felix@meyerwolters.de>
2018-02-17 22:16:55 +0100

    Fix bug with 'text' and 'json' switched.

--- auth_with/http.py
+++ auth_with/http.py
@@ -5,9 +5,9 @@ async def get(url, *args, return_type='text', **kwargs):
     """"""
     async with aiohttp.ClientSession() as session:
         async with session.get(url, *args, **kwargs) as result:
-            if return_type == 'text':
+            if return_type == 'json':
                 return await result.json()
-            elif return_type == 'json':
+            elif return_type == 'text':
                 return await result.text()
 
 
@@ -15,7 +15,7 @@ async def post(url, *args, return_type='text', **kwargs):
     """"""
     async with aiohttp.ClientSession() as session:
         async with session.post(url, *args, **kwargs) as result:
-            if return_type == 'text':
+            if return_type == 'json':
                 return await result.json()
-            elif return_type == 'json':
+            elif return_type == 'text':
                 return await result.text()
